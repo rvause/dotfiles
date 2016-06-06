ad74d34c360c57b964c805c719ecb6033974a7b6
Rick Vause <rvause@gmail.com>
2016-06-06 14:48:30 +0200

    Add ipython prompt config

---
+++ _ipython/profile_default/ipython_config.py
@@ -0,0 +1,6 @@
+c = get_config()
+
+c.PromptManager.out_template = '[\#] <<< '
+c.PromptManager.in_template = '[\#] >>> '
+c.PromptManager.in2_template = '> '
+c.PromptManager.color_scheme = 'Linux'
