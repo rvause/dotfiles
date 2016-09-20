ff5f34745bc00cf46dfb4a941abcc044ced2bc40
Rick Vause <rvause@gmail.com>
2016-09-20 12:59:40 +0200

    Add ipython 5.x compatible prompt config

--- _ipython/profile_default/ipython_config.py
+++ _ipython/profile_default/ipython_config.py
@@ -1,6 +1,20 @@
+from IPython import get_ipython
+from IPython.terminal.prompts import Prompts, Token
+
+
 c = get_config()
 
-c.PromptManager.out_template = '[\#] <<< '
-c.PromptManager.in_template = '[\#] >>> '
-c.PromptManager.in2_template = '> '
-c.PromptManager.color_scheme = 'Linux'
+
+class SimplePrompt(Prompts):
+    def in_prompt_tokens(self, cli=None):
+        return [(Token.Prompt, '[{}] >>> '.format(self.shell.execution_count))]
+
+    def out_prompt_tokens(self, cli=None):
+        return [(Token.Prompt, '[{}] <<< '.format(self.shell.execution_count))]
+
+    def continuation_prompt_tokens(self, cli=None, width=None):
+        return [(Token.Prompt, '    ... ')]
+
+
+c.TerminalInteractiveShell.prompts_class = SimplePrompt
+
